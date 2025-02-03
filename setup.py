bb19e7d473c6e0cf919494df42f8a9b4dc43411d
DaanJansen94 <jansendaan94@gmail.com>
2025-02-03 13:42:41 +0100

    .

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, find_packages
+import os
+import site
+import sys
+
+def _post_install(dir):
+    # Add bin to PATH during installation
+    path_file = os.path.join(dir, 'a3partitioner.pth')
+    with open(path_file, 'w') as f:
+        f.write(os.path.join(os.getcwd(), 'bin'))
 
 setup(
     name="a3partitioner",
     version="0.1.0",
     packages=find_packages(),
+    scripts=['bin/A3Partitioner'],
     install_requires=[
         "biopython>=1.80",
     ],
-    entry_points={
-        'console_scripts': [
-            'A3Partitioner=a3partitioner.cli:main',
-        ],
-    },
+    data_files=[
+        ('bin', ['bin/A3Partitioner']),
+    ],
     author="Daan Jansen",
     author_email="jansendaan94@gmail.com",
     description="A bioinformatics tool for creating APOBEC3 and non-APOBEC3 partitions",
@@ -24,3 +32,6 @@ setup(
     ],
     python_requires=">=3.6",
 )
+
+if 'install' in sys.argv:
+    _post_install(site.getsitepackages()[0])
